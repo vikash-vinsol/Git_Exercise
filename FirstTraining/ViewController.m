c5ef58ba8e2361828b0b438e18c41c6a5c5e9d93
vikash-vinsol <vikashsoni@Vikashs-MacBook-Air.local>
2014-07-07 18:12:18 +0530

    Changed number of section to 4

--- FirstTraining/ViewController.m
+++ FirstTraining/ViewController.m
@@ -29,7 +29,7 @@
 
 -(NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView
 {
-    return 6;
+    return 4;
 }
 
 -(NSInteger)collectionView:(UICollectionView *)view numberOfItemsInSection:(NSInteger)section
